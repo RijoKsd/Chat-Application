672b9c868b37f0a48a5a7a896335e2acaeade2b2
Rijo Sebastian <rijobdk@gmail.com>
2024-07-19 16:54:47 +0530

    feat: Integrate Socket.IO for real-time communication

---
+++ client/src/context/SocketContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useAuthContext } from "./AuthContext.jsx";
+import io from "socket.io-client";
+
+export const SocketContext = createContext();
+
+export const SocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const { authUser } = useAuthContext();
+
+  const [onlineUsers, setOnlineUsers] = useState([]);
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("http://localhost:5000", {
+        query: {
+          userId: authUser._id,
+        },
+      });
+      setSocket(socket);
+      // socket.on is used to listen for events from the server and client
+      socket.on("getOnlineUsers", (users) => {
+        setOnlineUsers(users);
+      });
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, []);
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export const useSocketContext = () => useContext(SocketContext);
--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 import express from "express";
-import dotenv from "dotenv";
+ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToDB from "./db/connectToDB.js";
 import cookieParser from "cookie-parser";
 
-const app = express();
+import { app ,server} from "./socket/socket.js";
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -21,7 +22,7 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToDB();
   console.log(`Server is running on port  ${PORT}`);
 });

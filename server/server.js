4f04dd05571f4d97697ca30c4e65fae8396ac7f7
Rijo Sebastian <rijobdk@gmail.com>
2024-07-19 20:57:06 +0530

    Build for deployment

--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToDB from "./db/connectToDB.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import { app ,server} from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 app.use(express.json());
@@ -17,9 +20,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+
+app.use(express.static(path.join(__dirname, "/client/dist")))
+
+app.get("*", (req,res)=>{
+  res.sendFile(path.join(__dirname,  "client", "dist", "index.html"));
+})
+ 
 
 
 server.listen(PORT, () => {

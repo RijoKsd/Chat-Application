8718a23f4bb66fa2c495a722c6144c0e302cbc2b
Rijo Sebastian <rijobdk@gmail.com>
2024-07-20 09:15:13 +0530

    Build frontent

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
+import path from "path";
 import express from "express";
- import dotenv from "dotenv";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToDB from "./db/connectToDB.js";
 import cookieParser from "cookie-parser";
-import path from "path";
 
-import { app ,server} from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,19 +15,16 @@ const __dirname = path.resolve();
 
 dotenv.config();
 app.use(express.json());
-app.use(cookieParser());;
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.use(express.static(path.join(__dirname, "/client/dist")))
-
-app.get("*", (req,res)=>{
-  res.sendFile(path.join(__dirname,  "client", "dist", "index.html"));
-})
- 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToDB();
